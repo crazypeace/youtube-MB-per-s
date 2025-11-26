98ae244e277995ae96fda080babe574145829ca3
crazypeace <txican@gmail.com>
2025-11-26 14:40:28 +0800

    Update YouTube 网速换算为 MB-s-2025-11-26.user.js

--- YouTube 网速换算为 MB-s-2025-11-26.user.js
+++ YouTube 网速换算为 MB-s-2025-11-26.user.js
@@ -5,7 +5,7 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://www.youtube.com/watch*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=blogger.com
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
 
